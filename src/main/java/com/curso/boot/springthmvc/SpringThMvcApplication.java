1e2600cbdca5f9bd14917ed2de8ab7533360ab3e
Ananias Raphael <ananias.nobrega@gmail.com>
2021-04-13 22:56:38 -0300

    init aplication

---
+++ src/main/java/com/curso/boot/springthmvc/SpringThMvcApplication.java
@@ -0,0 +1,13 @@
+package com.curso.boot.springthmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringThMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringThMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/curso/boot/springthmvc/SpringThMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.boot.springthmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringThMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
